0088271a0e51b652e7882f08a8948e36ecc5ac1d
MauZNinja <47656478+MauZNinja@users.noreply.github.com>
2020-03-01 21:02:54 -0700

    Added login and signup

---
+++ src/config/fire.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAujTBkecFLAA0iqwPgdBNs6Lvh8ufky8A",
+    authDomain: "team19-1b9b2.firebaseapp.com",
+    databaseURL: "https://team19-1b9b2.firebaseio.com",
+    projectId: "team19-1b9b2",
+    storageBucket: "team19-1b9b2.appspot.com",
+    messagingSenderId: "815111991687",
+    appId: "1:815111991687:web:03703be225a6901b26e322",
+    measurementId: "G-31W92D15ZQ"
+  };
+
+  const fire = firebase.initializeApp(firebaseConfig);
+
+  export default fire;
